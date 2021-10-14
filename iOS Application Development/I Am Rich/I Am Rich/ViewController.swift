cc4b5bbaed40b6f36e8e38fefdc3b5b571208369
KG Haarish Kannan <haarishkannanii@gmail.com>
2021-10-14 12:41:05 +0530

    initial commit for I Am Rich

---
+++ iOS Application Development/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Haarish on 14/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
